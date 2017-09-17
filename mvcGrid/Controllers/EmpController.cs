80adf951ed73fb954f6e44f3c60b675d23b5645b
prasad4edu <prasad4edu@gmail.com>
2017-09-17 22:02:00 +0530

    V2 17-9-2017

---
+++ mvcGrid/Controllers/EmpController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcGrid.Models;
+
+namespace mvcGrid.Controllers
+{
+    public class EmpController : Controller
+    {
+        //
+        // GET: /Emp/
+        EmpBusinessLayer objEmpBusinessLayer = new EmpBusinessLayer();
+        public ActionResult EmpList()
+        {
+            List<Empstructure> EmpList = objEmpBusinessLayer.getAllEmpDetails().ToList();
+            return View(EmpList);
+        }
+
+        [HttpGet]
+        [ActionName("EmpEdit")]
+        public ActionResult EmpEdit(string EmpId)
+        {
+            Empstructure EmpList = objEmpBusinessLayer.getEmpDetails(EmpId);
+            return View(EmpList);
+        }
+
+
+        [HttpPost]
+        [ActionName("EmpEdit")]
+        public ActionResult EmpEdit(Empstructure empStructure)
+        {
+            if (objEmpBusinessLayer.updateEmpDetails(empStructure) == 1)
+                return RedirectToAction("EmpList", "Emp");
+            return View();
+        }
+
+        public ActionResult EmpDetails(string EmpId)
+        {
+            Empstructure EmpList = objEmpBusinessLayer.getEmpDetails(EmpId);
+            return View(EmpList);
+        }
+
+        [HttpGet]
+        [ActionName("EmpDelete")]
+        public ActionResult EmpDelete()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("EmpDelete")]
+        public ActionResult EmpDelete(string ipEmpId)
+        {
+            if (objEmpBusinessLayer.deleteEmpDetails(ipEmpId) == 1)
+                return RedirectToAction("EmpList", "Emp");
+            return View();
+        }
+
+        [HttpGet]
+        [ActionName("EmpInsert")]
+        public ActionResult EmpInsert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("EmpInsert")]
+        public ActionResult EmpInsert(Empstructure empStructure)
+        {
+            if (objEmpBusinessLayer.insertEmpDetails(empStructure) == 1)
+                return RedirectToAction("EmpList", "Emp");
+            return View();
+        }
+    }
+}
---
+++ mvcGrid/Models/EmpBusinessLayer.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+namespace mvcGrid.Models
+{
+    public class EmpBusinessLayer
+    {
+        public IEnumerable<Empstructure> getAllEmpDetails()
+        {
+            List<Empstructure> empList = new List<Empstructure>();
+            try
+            {
+                string strConnection = ConfigurationManager.ConnectionStrings["CustomerDataConnectionString"].ConnectionString;
+                using (SqlConnection sqlCn = new SqlConnection(strConnection))
+                {
+                    SqlCommand sqlCmd = new SqlCommand("spGetAllEmpDetails", sqlCn);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCn.Open();
+                    SqlDataReader sqlDr = sqlCmd.ExecuteReader();
+                    while (sqlDr.Read())
+                    {
+                        Empstructure empstructure = new Empstructure();
+                        empstructure.EmpID = Convert.ToInt32(sqlDr["Empid"]);
+                        empstructure.EmpName = sqlDr["EmpName"].ToString();
+                        empstructure.EmpLastName = sqlDr["EmpLastName"].ToString();
+                        empstructure.EmpAge = Convert.ToInt32(sqlDr["EmpAge"]);
+                        empstructure.EmpAddress = sqlDr["EmpAddress"].ToString();
+                        empList.Add(empstructure);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            { }
+            return empList;
+        }
+
+        public Empstructure getEmpDetails(string ipEmpID)
+        {
+            Empstructure empstructure = new Empstructure();
+            try
+            {
+                string strConnection = ConfigurationManager.ConnectionStrings["CustomerDataConnectionString"].ConnectionString;
+                using (SqlConnection sqlCn = new SqlConnection(strConnection))
+                {
+                    SqlCommand sqlCmd = new SqlCommand("spGetEmpDetail", sqlCn);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@EmpID", ipEmpID);
+                    sqlCn.Open();
+                    SqlDataReader sqlDr = sqlCmd.ExecuteReader();
+                    while (sqlDr.Read())
+                    {
+                        
+                        empstructure.EmpID = Convert.ToInt32(sqlDr["Empid"]);
+                        empstructure.EmpName = sqlDr["EmpName"].ToString();
+                        empstructure.EmpLastName = sqlDr["EmpLastName"].ToString();
+                        empstructure.EmpAge = Convert.ToInt32(sqlDr["EmpAge"]);
+                        empstructure.EmpAddress = sqlDr["EmpAddress"].ToString();
+                      
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            { }
+            return empstructure;
+        }
+
+        public int updateEmpDetails(Empstructure empStructure)
+        {
+            int sqlint = 0;
+            try
+            {
+                string strConnection = ConfigurationManager.ConnectionStrings["CustomerDataConnectionString"].ConnectionString;               
+                using (SqlConnection sqlCn = new SqlConnection(strConnection))
+                {
+                    SqlCommand sqlCmd = new SqlCommand("spUpdateEmpDetail", sqlCn);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@EmpID", empStructure.EmpID);
+                    sqlCmd.Parameters.AddWithValue("@EmpName", empStructure.EmpName);
+                    sqlCmd.Parameters.AddWithValue("@EmpLastName", empStructure.EmpLastName);
+                    sqlCmd.Parameters.AddWithValue("@EmpAge", empStructure.EmpAge);
+                    sqlCmd.Parameters.AddWithValue("@EmpAddress", empStructure.EmpAddress);
+                    sqlCn.Open();
+                    sqlint = sqlCmd.ExecuteNonQuery();                   
+                }                
+            }
+            catch (Exception ex)
+            { }
+            return sqlint;
+        }
+
+        public int deleteEmpDetails(string ipEmpID)
+        {
+            int sqlint = 0;
+            try
+            {
+                string strConnection = ConfigurationManager.ConnectionStrings["CustomerDataConnectionString"].ConnectionString;
+                using (SqlConnection sqlCn = new SqlConnection(strConnection))
+                {
+                    SqlCommand sqlCmd = new SqlCommand("spDeleteEmpDetail", sqlCn);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@EmpID", ipEmpID);
+                    sqlCn.Open();
+                    sqlint = sqlCmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            { }
+            return sqlint;
+        }
+
+        public int insertEmpDetails(Empstructure empStructure)
+        {
+            int sqlint = 0;
+            try
+            {
+                string strConnection = ConfigurationManager.ConnectionStrings["CustomerDataConnectionString"].ConnectionString;
+                using (SqlConnection sqlCn = new SqlConnection(strConnection))
+                {
+                    SqlCommand sqlCmd = new SqlCommand("spInsertEmpDetail", sqlCn);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@EmpName", empStructure.EmpName);
+                    sqlCmd.Parameters.AddWithValue("@EmpLastName", empStructure.EmpLastName);
+                    sqlCmd.Parameters.AddWithValue("@EmpAge", empStructure.EmpAge);
+                    sqlCmd.Parameters.AddWithValue("@EmpAddress", empStructure.EmpAddress);
+                    sqlCn.Open();
+                    sqlint = sqlCmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            { }
+            return sqlint;
+        }
+    }
+}
